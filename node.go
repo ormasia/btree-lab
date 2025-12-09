62f217fc1596fb74fe71e355715b30f92267485c
ormasia <347820478@qq.com>
2025-12-09 10:59:34 +0800

    docs: 明确findIndex的含义

--- node.go
+++ node.go
@@ -1,5 +1,7 @@
 package btree
 
+// key 就在当前节点的 items 中，返回索引及 true
+// key 不在当前节点的 items 中，返回应该插入的位置索引及 false
 func (t *BTree[K, V]) findIndex(n *node[K, V], key K) (int, bool) {
 	i := 0
 	for i < len(n.items) && t.lessThan(n.items[i].key, key) {
